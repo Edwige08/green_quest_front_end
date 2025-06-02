063f518ccd8a92c04f7091b47da6b71b79b01d56
laanatri <laanatri.ade@gmail.com>
2025-06-02 11:50:10 +0200

    init login component and page

---
+++ src/app/components/organisms/LoginForm.jsx
@@ -0,0 +1,13 @@
+const InputLabel = require("../atoms/InputLabel")
+const Button = require("../atoms/Button")
+
+export default function LoginForm (props) {
+    return <>
+        <InputLabel/>
+        <InputLabel/>
+        <Button/>
+        
+        {'Créez votre compte'}
+    </>
+
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,7 @@
+const LoginForm = require("../components/organisms/LoginForm")
+
+export default function Login (props) {
+
+    <LoginForm/>
+
+}
