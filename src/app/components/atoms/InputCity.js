47590146f4d1b5df49b8ceafe175d726f71c71bd
VictorLpr <victorlepron@gmail.com>
2025-06-02 10:57:18 +0200

    add : input city with autocomplete

---
+++ src/app/components/atoms/InputCity.js
@@ -0,0 +1,74 @@
+import { useState, useEffect, useCallback } from 'react';
+
+function debounce(fn, delay) {
+    let timeoutId;
+    return (...args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn(...args), delay);
+    };
+}
+
+export default function InputCity({onSelect}) {
+    const [query, setQuery] = useState('');
+    const [villes, setVilles] = useState([]);
+    const [selectedCity, setSelectedCity] = useState("")
+
+    const fetchVilles = useCallback(async (search) => {
+        if (search.length < 2) {
+            setVilles([]);
+            return;
+        }
+        const res = await fetch(`https://geo.api.gouv.fr/communes?nom=${search}&limit=5&fields=centre`);
+        const data = await res.json();
+        console.log(data)
+        setVilles(data);
+    }, []);
+
+    const debouncedFetch = useCallback(debounce(fetchVilles, 300), [fetchVilles]);
+
+    useEffect(() => {
+        debouncedFetch(query);
+    }, [query, debouncedFetch]);
+
+    const handleSelect = (city) => {
+        setSelectedCity(city.nom);
+        onSelect({
+            name: city.nom,
+            code: city.code,
+            lat: city.centre?.coordinates[1] || [],
+            lng: city.centre?.coordinates[0] || [] 
+        })
+        setQuery("")
+        setVilles([]);
+    };
+
+    return (
+        <div className="relative ">
+            <input
+                type="text"
+                value={selectedCity || query}
+                onChange={e => {
+                    setQuery(e.target.value)
+                    setSelectedCity("")
+                }}
+                placeholder="Entrez une ville..."
+                className="border p-2 w-9/10"
+            />
+            {villes.length > 0 && (
+                <ul className="absolute bg-white rounded-md border w-9/10 z-10 ">
+                    {villes.map(ville => (
+                        <li
+                            key={ville.code}
+                            onClick={() => handleSelect(ville)}
+                            className="p-2 rounded-md hover:bg-(--text-secondary)   cursor-pointer input-city"
+                        >
+                            {ville.nom} ({ville.code})
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+
--- src/app/volunteers/profiles/page.js
+++ src/app/volunteers/profiles/page.js
@@ -1,7 +1,9 @@
+import VolunteerForm from "@/app/components/organisms/VolunteerForm";
+
 export default function Profiles () {
     return (
         <>
-        
+        <VolunteerForm ></VolunteerForm>
         </>
     );
 }
