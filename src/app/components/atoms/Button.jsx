b357316a32d7a42e2a3cd3b5b4b3fb191943da5c
laanatri <laanatri.ade@gmail.com>
2025-06-03 10:47:22 +0200

    add buttons, update page login and loginform

--- src/app/components/atoms/Button.jsx
+++ src/app/components/atoms/Button.jsx
@@ -1,5 +1,3 @@
-export default function Button (props) {
-    return (
-        <button>{props.icon} {props.text}</button>
-    )
+export default function Button ({ classes, type, onClick, lucide, text }) {
+    return <button className={"size-100 rounded-sm duration-300 ease-in-out cursor-pointer h-14 w-full flex justify-center items-center gap-2 " + classes} type={type} onClick={onClick}>{lucide} {text}</button>
 }
--- src/app/components/organisms/LoginForm.jsx
+++ src/app/components/organisms/LoginForm.jsx
@@ -1,13 +1,17 @@
-const InputLabel = require("../atoms/InputLabel")
-const Button = require("../atoms/Button")
+import InputLabel from "../atoms/InputLabel";
+import ButtonForm from "../atoms/ButtonForm";
 
-export default function LoginForm (props) {
-    return <>
-        <InputLabel/>
-        <InputLabel/>
-        <Button/>
-        
-        {'Créez votre compte'}
-    </>
+import  { LogIn } from "lucide-react";
 
+import Form from 'next/form';
+
+export default function LoginForm () {
+
+    let icon = <LogIn />;
+
+    return <Form action="">
+                <InputLabel />
+                <InputLabel />
+                <ButtonForm type="submit" classes="bg-(--primary-color) hover:bg-(--primary-color-hover)" lucide={icon} text="Se connecter"/>
+            </Form>
 }
