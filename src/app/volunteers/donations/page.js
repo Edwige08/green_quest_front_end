9e924a94ae7d70987206f323a06c6f778c4ea7a6
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-06-06 12:03:46 +0200

    edit: donations page

--- src/app/volunteers/donations/page.js
+++ src/app/volunteers/donations/page.js
@@ -1,16 +1,31 @@
+'use client'
 import AssociationsCards from "@/app/components/molecules/AssociationsCards";
 import { Gift } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function Donations() {
 
-    const collectedPoints = "500"
+    const [pointsData, setPointsData] = useState()
+
+    useEffect(() => {
+        async function getVolunteerPoints() {
+        const response = await fetch('http://localhost:5001/volunteers')
+        const data = await response.json()
+        console.log('❤️', data)
+        setPointsData(data)
+    }
+    getVolunteerPoints()
+    }, [])
+
+    const collectedPoints = pointsData?.data?.points ?? '...';
+
     return (
         <main>
-            <div className="flex flex-col items-center m-auto max-w-[28rem] bg-[white] border-(--border-color) border-0 rounded-lg shadow-lg">
+            <div className="flex flex-col items-center m-auto max-w-[28rem] border-(--border-color) border-0 rounded-lg shadow-lg">
                 <p className="text-center font-bold text-lg">Faire un don</p>
 
                 <p className="flex gap-2 text-(--primary-color) "> <Gift /> Points collectées : {collectedPoints}  </p>
-            
+
                 <div className="shadow-black m-3 ">
                     <AssociationsCards />
                 </div>
