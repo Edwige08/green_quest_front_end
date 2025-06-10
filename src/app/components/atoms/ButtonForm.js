9b4d37c8291fe8b59c03cf469ceb04fee1c8a58b
Edwige08 <edwige.saves@gmail.com>
2025-06-10 14:42:20 +0200

    fix: css on profile page and footer

--- src/app/components/atoms/ButtonForm.js
+++ src/app/components/atoms/ButtonForm.js
@@ -1,3 +1,3 @@
 export default function ButtonForm ({ classes, type, lucide, text, disabled = false }) {
-    return <button className={"size-100 rounded-sm duration-300 ease-in-out cursor-pointer h-14 w-full flex justify-center items-center gap-2 " + classes} type={type} disabled={disabled}>{lucide} {text}</button>
+    return <button className={"size-100 rounded-lg duration-300 ease-in-out cursor-pointer h-14 w-full flex justify-center items-center gap-2 " + classes} type={type} disabled={disabled}>{lucide} {text}</button>
 }
