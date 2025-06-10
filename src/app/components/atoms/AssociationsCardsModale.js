d97eea3e1d68c1d0e5437a5b2a5fc25c04142267
VictorLpr <victorlepron@gmail.com>
2025-06-10 14:36:31 +0200

    update : donation modal max points

--- src/app/components/atoms/AssociationsCardsModale.js
+++ src/app/components/atoms/AssociationsCardsModale.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Button from "./Button";
 
-export default function AssociationsCardsModale({ onClose, associationName }) {
+export default function AssociationsCardsModale({ onClose, associationName,points }) {
     const [donationMade, setDonationMade] = useState(false);
     const [donation, setDonation] = useState(0);
 
@@ -21,7 +21,7 @@ export default function AssociationsCardsModale({ onClose, associationName }) {
                     <div className="bg-white p-3 rounded shadow-lg w-full flex flex-col items-center gap-3">
                         <div>
 
-                            <input type="number" step={100} placeholder="100 ?" list="defaultNumbers" min={0} className="border w-20 p-1 rounded-md" onChange={handleChange}></input>
+                            <input type="number" step={100} placeholder="100 ?" list="defaultNumbers" min={0} max={points} className="border w-20 p-1 rounded-md" onChange={handleChange}></input>
                             points
                         </div>
 
