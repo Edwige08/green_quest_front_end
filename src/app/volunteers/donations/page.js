1916c764c2fc400e76c5affb6609ddfab3f6efcc
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-06-10 11:27:21 +0200

    remove: localStorage from the page

--- src/app/volunteers/donations/page.js
+++ src/app/volunteers/donations/page.js
@@ -1,31 +1,18 @@
 'use client'
+
+import Points from "@/app/components/atoms/Points";
 import AssociationsCards from "@/app/components/molecules/AssociationsCards";
-import { Gift } from "lucide-react";
-import { useEffect, useState } from "react";
 
 export default function Donations() {
 
-    const [pointsData, setPointsData] = useState()
 
-    useEffect(() => {
-        async function getVolunteerPoints() {
-        const response = await fetch('http://localhost:5001/volunteers')
-        const data = await response.json()
-        console.log('❤️', data)
-        setPointsData(data)
-    }
-    getVolunteerPoints()
-    }, [])
-
-    const collectedPoints = pointsData?.data?.points ?? '...';
 
     return (
         <main>
             <div className="flex flex-col items-center m-auto max-w-[28rem] bg-(--background) border-(--border-color) border-0 rounded-lg shadow-lg">
                 <p className="text-center font-bold text-lg">Faire un don</p>
 
-                <p className="flex gap-2 text-(--primary-color) "> <Gift /> Points collectées : {collectedPoints}  </p>
-
+                <Points />
                 <div className="shadow-black m-3 ">
                     <AssociationsCards />
                 </div>
