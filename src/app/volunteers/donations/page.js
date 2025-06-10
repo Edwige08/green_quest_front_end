a977263752aeae949556193616e098d64d20cbe8
VictorLpr <victorlepron@gmail.com>
2025-06-10 11:35:28 +0200

    remove points

--- src/app/volunteers/donations/page.js
+++ src/app/volunteers/donations/page.js
@@ -1,18 +1,33 @@
-'use client'
+"use client"
 
-import Points from "@/app/components/atoms/Points";
 import AssociationsCards from "@/app/components/molecules/AssociationsCards";
+import { useState, useEffect } from "react";
+import { Gift } from "lucide-react";
 
 export default function Donations() {
 
 
+    const profileUserName = localStorage.getItem("currentUserName");   
+       
+        const [pointsData, setPointsData] = useState()
+    
+        async function getVolunteerPoints() {
+            const response = await fetch(`http://localhost:5001/volunteers/${profileUserName}`)
+            const data = await response.json()
+            console.log('❤️', data)
+            setPointsData(data.points)
+        }
+        useEffect(() => {
+            
+        getVolunteerPoints()
+        }, [])
 
     return (
         <main>
             <div className="flex flex-col items-center m-auto max-w-[28rem] bg-(--background) border-(--border-color) border-0 rounded-lg shadow-lg">
                 <p className="text-center font-bold text-lg">Faire un don</p>
 
-                <Points />
+            <p className="flex gap-2 text-(--primary-color) "> <Gift /> Points collectées : {pointsData}  </p>
                 <div className="shadow-black m-3 ">
                     <AssociationsCards />
                 </div>
