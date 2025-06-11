227a10064f4b1c65545e6aa118fde3c7c3882819
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-06-11 12:23:30 +0200

    create: registration page

--- src/app/registration/page.jsx
+++ src/app/registration/page.jsx
@@ -4,13 +4,14 @@ import Button from '../components/atoms/Button';
 import { LogIn } from "lucide-react";
 
 import { useRouter } from 'next/navigation';
+import VolunteerForm from '../components/organisms/VolunteerForm';
 
 export default function registrationPage () {
     const router = useRouter();
 
     return (
         <>
-            <p>Vous ne pouvez pas vous inscrire pour le moment. <br /> Veuillez réessayer plus tard :\)</p>
+            <VolunteerForm />
             <Button classes="bg-(--primary-color) hover:bg-(--primary-color-hover)" type="button" onClick={() => {router.push('/login')}} lucide={<LogIn />} text="Se connecter"/>
         </>
     )
